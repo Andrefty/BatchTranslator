5f0e4caa889d3fe962db6f6b04aedf754626d724
a <39839328+Andrefty@users.noreply.github.com>
2022-12-16 01:25:15 +0200

    a template for backend

--- backendscript.py
+++ backendscript.py
@@ -1,7 +1,8 @@
 import io
+import os
 import zipfile
 
-from argos_translate import ArgosTranslate
+from azure.cognitiveservices.language.translator import TranslatorClient
 from azure.storage.blob import BlockBlobService
 
 
@@ -11,9 +12,10 @@ blob_service = BlockBlobService(
     account_key="your_storage_account_key"
 )
 
-# create an ArgosTranslate instance
-argos_translate = ArgosTranslate(
-    api_key="your_argos_translate_api_key"
+# create a TranslatorClient instance
+translator_client = TranslatorClient(
+    endpoint="your_azure_translator_endpoint",
+    credential="your_azure_translator_key"
 )
 
 # download the zip file from Azure Blob Storage
@@ -25,8 +27,8 @@ zip_file_contents = blob_service.get_blob_to_bytes(
 # open the zip file
 zip_file = zipfile.ZipFile(io.BytesIO(zip_file_contents))
 
-# extract the files from the zip file
-zip_file.extractall()
+# create a new zip file to store the translated files
+translated_zip_file = zipfile.ZipFile("translated_files.zip", "w")
 
 # translate the contents of each file to English
 for file_name in zip_file.namelist():
@@ -34,12 +36,35 @@ for file_name in zip_file.namelist():
     file_contents = zip_file.read(file_name)
 
     # translate the contents of the file to English
-    translated_contents = argos_translate.translate(
+    translated_contents = translator_client.translate(
         text=file_contents,
-        source_language="auto",
-        target_language="en"
-    )
+        to_language="en"
+    ).text
 
     # write the translated contents to a new file
-    with open(file_name, "w") as file:
+    translated_file_name = f"{file_name}.en"
+    with open(translated_file_name, "w") as file:
         file.write(translated_contents)
+
+    # add the translated file to the translated zip file
+    translated_zip_file.write(translated_file_name)
+
+    # delete the translated file
+    os.remove(translated_file_name)
+
+# close the translated zip file
+translated_zip_file.close()
+
+# upload the translated zip file to Azure Blob Storage
+blob_service.create_blob_from_path(
+    container_name="your_translated_container_name",
+    blob_name="your_translated_blob_name",
+    file_path="translated_files.zip"
+)
+
+# delete the original zip file
+blob_service.delete_blob(
+    container_name="your_container_name",
+    blob_name="your_blob_name"
+)
+
