f46ec1c4f5318afa8a7dac62df1dbd92c0b4a815
a <39839328+Andrefty@users.noreply.github.com>
2022-12-15 02:18:35 +0200

    If email field blank email+filename not sent to db

--- uiproject/uiapp/models.py
+++ uiproject/uiapp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Emailaddress(models.Model):
-    email = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100,blank=True)
     # language chooser
     # language = models.CharField(max_length=2, choices=[('en', 'English'), ('de', 'German')], default='en')
     filename = models.CharField(max_length=100,blank=True)
--- uiproject/uiapp/views.py
+++ uiproject/uiapp/views.py
@@ -12,21 +12,28 @@ class EmailaddressCreateView(CreateView):
     model = Emailaddress
     form_class = EmailaddressForm
     success_url = reverse_lazy('uiapp:emailaddress_create')
+
     def form_valid(self, form):
         # https://storagetexte.blob.core.windows.net/uploadzip?sp=racwdli&st=2022-12-12T22:56:11Z&se=2023-02-01T06:56:11Z&spr=https&sv=2021-06-08&sr=c&sig=5PU8S07Bywy85cfd24b3BLhemdICje7ucRKxVM%2Fq720%3D
         try:
-            blob_service_client = BlobServiceClient.from_connection_string("***REMOVED***")
-            container_name = "uploadzip"
-            container_client = blob_service_client.get_container_client(container_name)
             if form.cleaned_data["uploadfile"].content_type != 'application/zip':
                 raise forms.ValidationError('The uploaded file must be a zip file')
-            print("fromcleaneddata",form.cleaned_data["filename"])
-            blob_client = container_client.upload_blob(data=form.cleaned_data["uploadfile"], name=form.cleaned_data["filename"])
+            blob_service_client = BlobServiceClient.from_connection_string(
+                "***REMOVED***")
+            container_name = "uploadzip"
+            container_client = blob_service_client.get_container_client(
+                container_name)
+            # print("fromcleaneddata",form.cleaned_data["filename"])
+            blob_client = container_client.upload_blob(
+                data=form.cleaned_data["uploadfile"], name=form.cleaned_data["filename"])
+            if form.cleaned_data["email"] == '':
+                return super().form_invalid(form)
             self.object = form.save()
             return super().form_valid(form)
         except forms.ValidationError as e:
             form.add_error('uploadfile', e)
             return super().form_invalid(form)
+
 
 """
 from django.views.generic import View
