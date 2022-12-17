63a5646cb05d70340cffbd7c7f7d7029d6e36e45
a <39839328+Andrefty@users.noreply.github.com>
2022-12-17 02:30:50 +0000

    unzip working

--- backendscript.py
+++ backendscript.py
@@ -2,20 +2,15 @@ import os
 import zipfile
 from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 
-# Replace with your Azure Storage account name and key
-account_name = "storagetexte"
-account_key = "2x2yhYM0XmQ+nE25iuU02lDZ7yaHPZ3rL0nFbEJtdnf0kL/4LRgvookhRXgwq8ZivOzHVMBIcmy9+ASt3uO4Mg=="
+# workpath=
 
 # Create the BlobServiceClient object
-service_client = BlobServiceClient(
-    f"https://{account_name}.blob.core.windows.net",
-    credential=account_key,
-)
+service_client = BlobServiceClient.from_connection_string(
+                "***REMOVED***")
 
 # Create the container client
 container_name = "uploadzip"
-container_client = ContainerClient(service_client, container_name)
-
+container_client = service_client.get_container_client(container_name)
 # List the blobs in the container
 blobs = container_client.list_blobs()
 
@@ -26,7 +21,7 @@ while blobs:
         # Do something with the blob, such as download it
         # Replace "local_path" with the desired local file path
         with open(blob.name, "wb") as f:
-            blob_client = BlobClient(service_client, container_name, blob.name)
+            blob_client = container_client.get_blob_client(blob.name)
             f.write(blob_client.download_blob().readall())
 
         # Unzip the file
